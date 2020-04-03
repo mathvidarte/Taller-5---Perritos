24e0135ccbe1ab5cbddd4c8d7dbbbc083ba0401c
Matheus Vidarte Sandoval <matheusvidartesandoval@192.168.1.52>
2020-04-03 12:55:27 -0500

    terminé

--- Desktop/Taller 5 - Perritos/Perritos/src/Model/Logic.java
+++ Desktop/Taller 5 - Perritos/Perritos/src/Model/Logic.java
@@ -1,6 +1,9 @@
 package Model;
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Collections;
 import java.util.LinkedList;
 
@@ -71,21 +74,62 @@ public class Logic {
 			
 			/*for (int j = 0; j < dog.size(); j++) {
 				System.out.println(dog.get(j).getId());
+			
 			}*/
 			break;
 		case 'b':
 			Collections.sort(dog, breedDog);
+			try {
+                printReport();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
 			break;
 		case 'd':
 			Collections.sort(dog, dateDog);
+			try {
+                printReport();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
 			break;
 		case 'n' :
 			Collections.sort(dog, nameDog);
+			try {
+                printReport();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
 			break;
+			
 		case 'a' :
 			Collections.sort(dog, ageDog);
+			try {
+                printReport();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
 		}
 	}
+	
+	public void printReport () throws FileNotFoundException {
+		PrintWriter pw = new PrintWriter (new File("data/datosNuevo.txt"));
+		pw.print(info());
+		pw.close();
+	}
+	
+	public String info() {
+		String myInfo = "";
+		for ( int i = 0; i < dog.size(); i++) {
+			Dog d = dog.get(i);
+			String msg = d.getId() + " " + d.getName().toLowerCase() + " " + d.getDate() + " " + d.getBreed().toLowerCase() + " " + d.getAge();;
+			myInfo += msg + "/n";
+		}
+		return myInfo;
+		
+	}
+	
+
 	
 }
 	
--- Desktop/Taller 5 - Perritos/Perritos/src/View/Main.java
+++ Desktop/Taller 5 - Perritos/Perritos/src/View/Main.java
@@ -37,5 +37,7 @@ public class Main extends PApplet {
 		createMain.sortListController(key);
 	}
 	
+	
+	
 
 }
